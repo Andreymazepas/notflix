5714b62146dca574978546f5feba084b608dc0b3
C1311295 Andrey Emmanuel Matrosov Mazepas <andrey.mazepas@ewave.com.br>
2020-03-15 13:05:46 -0300

    Escondendo StatusBar

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, ActivityIndicator} from 'react-native';
+import {StyleSheet, StatusBar} from 'react-native';
 import {Container} from 'native-base';
 import AppHeader from './Components/AppHeader';
 import ContentRow from './Components/ContentRow';
@@ -8,20 +8,12 @@ import AppFooter from './Components/AppFooter';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {isLoading: false};
   }
 
   render() {
-    if (this.state.isLoading) {
-      return (
-        <View style={{flex: 1, padding: 20}}>
-          <ActivityIndicator />
-        </View>
-      );
-    }
-
     return (
       <Container style={{backgroundColor: 'black'}}>
+        <StatusBar hidden />
         <AppHeader />
         <ContentRow
           fetchURL="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=dc0c0ec1c215e3f8594b2471ce751d65"
